acaf4a5b03b09baf419e1f0b24eb504f0ba75f39
SupRemaZie <Damien.Porchet.Etu@univ-lemans.fr>
2024-02-14 10:33:15 +0100

    init2

--- src/main/java/org/example/CompteBancaire.java
+++ src/main/java/org/example/CompteBancaire.java
@@ -1,20 +1,84 @@
 package org.example;
 
+import java.util.UUID;
+
 public class CompteBancaire {
 
-    private final int numeroCompte;
-    private  final Personne titulaire;
+    private final int numCompte = Integer.parseInt(UUID.randomUUID().toString());
+    private final Personne titulaire;
     private double solde;
 
-    public CompteBancaire(int numero, Personne titulaire, double solde) {
-        this.numeroCompte = numero;
+    private double debitMax = 1000.0;
+    private boolean isDecouvert = false;
+
+    private double decouvertMax = 800.0;
+
+    public CompteBancaire( Personne titulaire, double decouvertMax, double debitMax)  {
         this.titulaire = titulaire;
-        this.solde = solde;
+        this.solde = 0;
+        this.decouvertMax = decouvertMax;
+        this.debitMax = debitMax;
     }
 
 
+    public void crediter(double montant) {
+        if( montant > 0){
+            throw new IllegalArgumentException("Depot impossible, montant negatif");
+        }
+        this.solde += montant;
 
+    }
 
+    public void debiter(double montant) {
+        if  (montant > this.debitMax) {
+            throw new IllegalArgumentException("Retrait impossible, montant superieur au debit max");
+        }
+        if (solde <0) {
+            this.isDecouvert = true;
+            System.out.println("Vous êtes à decouvert de " + solde + "€ vous pouvez retirer jusqu'à " + decouvertMax + "€");
+        }
+       if (this.solde - montant < -this.decouvertMax) {
+           throw new IllegalArgumentException("Retrait impossible, decouvert max atteint");
+         }
 
+        this.solde -= montant;
+    }
 
+    public double getSold() {
+        return this.solde;
+    }
+
+    public String getTitulaire() {
+        return this.titulaire.toString();
+    }
+
+    public double setDecouvertMax(double decouvertMax) {
+        if(decouvertMax < 0) {
+            throw new IllegalArgumentException("Le decouvert max ne peut pas être négatif");
+        }
+        return this.decouvertMax;
+    }
+
+    public double setDebitMax(double debitMax) {
+        if(debitMax < 0) {
+            throw new IllegalArgumentException("Le debit max ne peut pas être négatif");
+        }
+        return this.debitMax;
+    }
+    /**
+     * @return all the informations about the account
+     */
+    @Override
+    public String toString() {
+        return "CompteBancaire{" +
+                "numCompte=" + numCompte +
+                ", titulaire=" + titulaire +
+                ", solde=" + solde +
+                ", debitMax=" + debitMax +
+                ", isDecouvert=" + isDecouvert +
+                ", decouvertMax=" + decouvertMax +
+                '}';
+    }
 }
+
+
--- src/main/java/org/example/Personne.java
+++ src/main/java/org/example/Personne.java
@@ -18,5 +18,37 @@ public class Personne {
         this.comptes = new ArrayList<>();
     }
 
+    public void deposer(double montant, CompteBancaire compte) {
+        compte.crediter(montant);
+    }
+
+    public void retirer(double montant, CompteBancaire compte) {
+
+        compte.debiter(montant);
+    }
+
+    public void ajouterCompte(CompteBancaire compte) {
+        this.comptes.add(compte);
+    }
+
+    public void supprimerCompte(CompteBancaire compte) {
+        this.comptes.remove(compte);
+    }
+
+    public ArrayList<CompteBancaire> getComptes() {
+        return this.comptes;
+    }
+    public void faireUnVirement(double montant, CompteBancaire compteDebit, CompteBancaire compteCredit) {
+        compteDebit.debiter(montant);
+        compteCredit.crediter(montant);
+    }
+
+
+
+
+
+
+
+
 
 }
